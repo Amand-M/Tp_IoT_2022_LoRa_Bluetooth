6a8f9aa75864a61a9837ec2cafddc3e8917b02bd
Amand-M <133797914+Amand-M@users.noreply.github.com>
2024-02-22 17:31:50 +0100

    Update sender.c

--- sender.c
+++ sender.c
@@ -11,8 +11,9 @@
 #define SS 18 // GPIO18 -- SX127x's CS
 #define RST 14 // GPIO14 -- SX127x's RESET
 #define DI0 26 // GPIO26 -- SX127x's IRQ(Interrupt Request)
+
 #define freq 868E6
-#define sf 8
+#define sf 7
 #define sb 125E3
 
 //Display
@@ -39,6 +40,8 @@ char *param = "";
 
 //Constantes LoRa
 float d1 = 0;
+float d2 = 0;
+float rssi = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -46,15 +49,14 @@ PubSubClient client(espClient);
 union pack
   {
     uint8_t frame[16]; // trames avec octets
-    float data; // 4 valeurs en virgule flottante
-  } sdp ; // paquet d’émission
+    float data[4]; // 4 valeurs en virgule flottante
+  } rdp;
 
 void setup() {
   // Set software serial baud to 115200;
   Serial.begin(9600);
 
   //Display
-
   u8x8.begin(); // initialize OLED
   u8x8.setFont(u8x8_font_chroma48medium8_r);
 
@@ -95,13 +97,15 @@ void setup() {
 
   LoRa.setSpreadingFactor(sf);
   LoRa.setSignalBandwidth (sb);
+  //LoRa.onReceive(onReceive); // pour indiquer la fonction ISR
+  //LoRa.receive(); // pour activer l'interruption (une fois)
 
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
     Serial.print("Message arrivé dans le topic: ");
     Serial.println(topic);
-    Serial.print("Message:");
+    Serial.print("Message: ");
     for (int i = 0; i < length; i++) {
         Serial.print((char) payload[i]);
     }
@@ -109,26 +113,56 @@ void callback(char *topic, byte *payload, unsigned int length) {
     Serial.println("-----------------------");
 }
 
+void mqtt5sec(){
+  int time;
+  time = millis() + 5000;
+  param = "868000000;7;125000";
+
+  while(1)
+  {  
+    if(millis()>time){
+    u8x8.clear();
+
+    client.publish(topic, param);
+    Serial.println("Message MQTT envoyé");
+    u8x8.drawString(0,1,"Message MQTT :");
+    u8x8.drawString(0,2,param);
+
+    time = millis() + 5000;
+    }  
+  };
+
+}
 
 void loop() {
-  //Partie MQTT
-  param = "param;jvas;868E6;7;125E3";
-  // param = "param;" + freq + ";" + sf + ";" + sb;
-  // snprintf(param, sizeof(param), "param;%s;%s;%s;%s", groupe, freq, sf, sb );
-
-  client.publish(topic, param);
-  Serial.println("Message envoyé");
  
   //émetteur LoRa
 
-  Serial.printf("\nNouveau Paquet : %f\n",d1) ;
-  LoRa.beginPacket(); // start packet
-    sdp.data=d1;
-    LoRa.write(sdp.frame,16);
-  LoRa.endPacket();
-  d1++;
-  
+  // Serial.printf("\nNouveau Paquet LoRa envoyé : %f\n",d1) ;
+  // u8x8.drawString(0,3,"Message LoRa :");
+  // // u8x8.drawString(0,4,d1);
+  // LoRa.beginPacket(); // start packet
+  //   sdp.data=d1;
+  //   LoRa.write(sdp.frame,16);
+  // LoRa.endPacket();
+  // d1++;
+
+  int packetLen;
+  packetLen=LoRa.parsePacket();
+
+  if(packetLen>0) {
+    int i=0;
+    while (LoRa.available()) {
+      rdp.frame[i]=LoRa.read();i++;
+    }
+    d1=rdp.data[0];
+    d2=rdp.data[1];
+
+    rssi=LoRa.packetRssi(); // force du signal en réception en dB
+    Serial.println(d1); Serial.println(d2);
+    Serial.printf("RSSI : ");
+    Serial.println(rssi);
+  }
 
   client.loop();
-  delay(5000);
 }
